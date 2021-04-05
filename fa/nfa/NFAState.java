36f2dccd2c63f44cbdaf5d1c8205120607d052b9
aidanleuck <aidan@leucks.com>
2021-04-05 17:29:11 -0600

    Added NFAState JavaDocs

--- fa/nfa/NFAState.java
+++ fa/nfa/NFAState.java
@@ -8,46 +8,77 @@ import java.util.Set;
 import fa.State;
 import fa.dfa.DFAState;
 
-public class NFAState extends State{
-  private boolean isFinal;
-  private HashMap<Character, LinkedHashSet<NFAState>> transitions;
+/**
+ * Implements a NFAState object.
+ * 
+ * @author Aidan Leuck, Zach Sherwood
+ */
+public class NFAState extends State {
+  private boolean isFinal; // Keeps track if the class is final
+  private HashMap<Character, LinkedHashSet<NFAState>> transitions; // Keeps track of transitions to other states
 
-  public NFAState(String name){
+  /**
+   * Constructor for NFAState, instantiates variables at creation
+   * 
+   * @param name - name of the NFAState
+   */
+  public NFAState(String name) {
     super.name = name;
     isFinal = false;
     transitions = new HashMap<Character, LinkedHashSet<NFAState>>();
   }
 
-  public void addTransition(char onSymb, NFAState to){
-        LinkedHashSet<NFAState> current = transitions.get(onSymb);
-        if(current == null){
-            current = new LinkedHashSet<NFAState>();
-         
-         
-        }
-        current.add(to);
-        transitions.put(onSymb, current);
-   
+  /**
+   * Adds a transition to the transition set
+   * 
+   * @param onSymb - Symbol to transition on
+   * @param to     - Which state to transition to
+   */
+  public void addTransition(char onSymb, NFAState to) {
+    LinkedHashSet<NFAState> current = transitions.get(onSymb);
+    if (current == null) {
+      current = new LinkedHashSet<NFAState>();
+
+    }
+    current.add(to);
+    transitions.put(onSymb, current);
+
   }
 
-  public HashMap<Character,LinkedHashSet<NFAState>> getTransition()
-	{
-		return transitions;
-	}
+  /**
+   * 
+   * @return - The transitions for a particular state
+   */
+  public HashMap<Character, LinkedHashSet<NFAState>> getTransition() {
+    return transitions;
+  }
 
-  public void setFinal(){
-      this.isFinal = true;
+  /**
+   * Sets a state as final
+   */
+  public void setFinal() {
+    this.isFinal = true;
   }
-  public boolean isFinal(){
-      return isFinal;
-  }
-  public Set<NFAState> getTo(char symb){
-      
-      LinkedHashSet<NFAState> returnState = transitions.get(symb);
-      return returnState;
 
-      
+  /**
+   * 
+   * @return true if state is final, false otherwise
+   */
+  public boolean isFinal() {
+    return isFinal;
   }
-  
-	
+
+  /**
+   * Returns possible paths from this state to another on a certain character
+   * 
+   * @param symb - character to transition from
+   * @return - The set of states that can be reached on the symbol
+   */
+  public Set<NFAState> getTo(char symb) {
+
+    LinkedHashSet<NFAState> returnState = transitions.get(symb);
+    return returnState;
+
+  }
+
 }
